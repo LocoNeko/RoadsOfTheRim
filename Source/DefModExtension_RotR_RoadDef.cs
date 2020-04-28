99b2ac55cd97b8d20169889679f1352f486348a1
loconeko <loconeko73@gmail.com>
2020-04-28 17:17:28 +0800

    try again little grasshoper

--- Source/DefModExtension_RotR_RoadDef.cs
+++ Source/DefModExtension_RotR_RoadDef.cs
@@ -121,7 +121,7 @@ namespace RoadsOfTheRim
             {
                 return true;
             }
-            return biomeHere.allowRoads;
+            return !biomeHere.impassable;
         }
     }
 }

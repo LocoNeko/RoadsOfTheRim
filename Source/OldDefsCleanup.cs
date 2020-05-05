9a54a7672cface42ad7c877e75c6287d96056935
loconeko <loconeko73@gmail.com>
2020-05-05 18:19:37 +0800

    handled this with a small ThingComp

--- Source/OldDefsCleanup.cs
+++ Source/OldDefsCleanup.cs
@@ -7,21 +7,13 @@ using Verse;
 
 namespace RoadsOfTheRim
 {
-    public class RotR_ISR2G : ThingWithComps
-    {
-        public override void Tick()
-        {
-            base.Tick();
-            RoadsOfTheRim.DebugLog("AN ISR2G just ticked at " + this.Position.ToString());
-        }
-    }
 
-    public class RotR_AISR2G : ThingWithComps
+    public class OldDefsCleanupComp : ThingComp
     {
-        public override void Tick()
+        public override void CompTick()
         {
-            base.Tick();
-            RoadsOfTheRim.DebugLog("AN ISR2G just ticked at " + this.Position.ToString());
+            RoadsOfTheRim.DebugLog("AN ISR2G just ticked at " + this.parent.Position.ToString());
         }
+
     }
 }

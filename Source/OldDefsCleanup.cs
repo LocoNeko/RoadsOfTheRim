5acf00a750e72d87e8964717a704e96df00294f4
loconeko <loconeko73@gmail.com>
2020-05-05 18:26:38 +0800

    progress

--- Source/OldDefsCleanup.cs
+++ Source/OldDefsCleanup.cs
@@ -12,8 +12,21 @@ namespace RoadsOfTheRim
     {
         public override void CompTick()
         {
-            RoadsOfTheRim.DebugLog("AN ISR2G just ticked at " + this.parent.Position.ToString());
+            Thing oldISR2G = this.parent ;
+            int level = 0;
+            if (oldISR2G.def.defName == "RotR_ISR2G")
+            {
+                level = 1;
+            }
+            else if (oldISR2G.def.defName == "RotR_AISR2G")
+            {
+                level = 2;
+            }
+            if (level > 0)
+            {
+                RoadsOfTheRim.DebugLog("Destroying a ISR2G level " + level + " at position "  + oldISR2G.Position.ToString());
+                oldISR2G.Destroy();
+            }
         }
-
     }
 }

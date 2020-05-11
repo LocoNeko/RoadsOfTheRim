99cc19828a35254bafe90a71a58a8c3bc61d2cbf
loconeko <loconeko73@gmail.com>
2020-05-11 14:56:22 +0800

    ahah

--- Source/ThingCompProperties_RotR_Vehicles.cs
+++ Source/ThingCompProperties_RotR_Vehicles.cs
@@ -7,7 +7,7 @@ namespace RoadsOfTheRim
     {
         public ThingCompProperties_RotR_Vehicles()
         {
-            this.compClass = typeof(ThingCompProperties_RotR_Vehicles);
+            this.compClass = typeof(ThingComp_RotR_Vehicles);
         }
 
         public ThingCompProperties_RotR_Vehicles(Type compClass) : base(compClass)
@@ -16,11 +16,9 @@ namespace RoadsOfTheRim
         }
 
         public int Seats = 0;
-        /*
         public bool OffRoad = false;
         public float Speed = 0f;
         public int Fuel = 0;
-        */
     }
 
     class ThingComp_RotR_Vehicles : ThingComp
@@ -35,15 +33,13 @@ namespace RoadsOfTheRim
         }
 
         public int Seats => properties.Seats;
-        /*
         public bool OffRoad => properties.OffRoad;
         public float Speed => properties.Speed;
         public int Fuel => properties.Fuel;
-        */
 
         public override void CompTick()
         {
-            //RoadsOfTheRim.DebugLog("Tick on a vehicle : "+parent.Label+" "+Seats+" seats, speed "+Speed+", "+(OffRoad?"":"not")+" offroad. Fuel : "+Fuel );
+            RoadsOfTheRim.DebugLog("Tick on a vehicle : "+parent.Label+" "+Seats+" seats, speed "+Speed+", "+(OffRoad?"":"not")+" offroad. Fuel : "+Fuel );
         }
 
     }

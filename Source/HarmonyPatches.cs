39378bf7fb84e05abf6c4e26b09c20bc8008a25e
loconeko <loconeko73@gmail.com>
2020-05-13 07:44:14 +0800

    more

--- Source/HarmonyPatches.cs
+++ Source/HarmonyPatches.cs
@@ -510,12 +510,7 @@ namespace RoadsOfTheRim
             for (int i = 0; i < codes.Count; i++)
             {
                 string s = codes[i].operand as string;
-                if (s.Contains("Impassable"))
-                {
-                    index = i;
-                    RoadsOfTheRim.DebugLog("Transpiling WorldPathFinder, found Impassable at index "+i);
-                    break;
-                }
+                RoadsOfTheRim.DebugLog("transpiling: "+s);
             }
             return codes.AsEnumerable();
         }

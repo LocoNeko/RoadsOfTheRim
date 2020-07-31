efd1ef4a4d4422609ed1d7def79377f45528f121
loconeko <loconeko73@gmail.com>
2020-07-31 12:53:00 +0800

    commands with an s

--- Source/BuildRoadWidget.cs
+++ Source/BuildRoadWidget.cs
@@ -12,7 +12,7 @@ namespace RoadsOfTheRim
 
 		private const float Size = 64f;
 
-		private static readonly Texture2D BuildRoadTex = ContentFinder<Texture2D>.Get("UI/Command/AddConstructionSite");
+		private static readonly Texture2D BuildRoadTex = ContentFinder<Texture2D>.Get("UI/Commands/AddConstructionSite");
 
 		public static void BuildRoadOnGUI(ref float curBaseY)
 		{
